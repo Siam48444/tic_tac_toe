6b28f4e4ac0d0936cd5cd0752f1a792c7a7b9f2c
Siam <siam48444@gmail.com>
2025-02-18 17:32:21 +0600

    siam

--- Assets/Sounds/sounds.js
+++ Assets/Sounds/sounds.js
@@ -4,15 +4,6 @@ export const click_sound_O = new Audio("./Assets/Sounds/click_sound_O.mp3");
 
 
 
-// Function to play the game
-export function play_sound(sound, is_muted=false) {
-    if ( !is_muted ) {
-        sound.play();
-    }
-}
-
-
-
 // Let the user choose the sound system
 export let is_muted = false;
 export const sound_button = document.querySelector(".sound_button");
@@ -20,4 +11,13 @@ export const sound_button = document.querySelector(".sound_button");
 export function toggle_sound() {
     sound_button.classList.toggle("muted");
     is_muted = !is_muted;
+}
+
+
+
+// Function to play the game
+export function play_sound(sound, is_muted=false) {
+    if ( !is_muted ) {
+        sound.play();
+    }
 }
