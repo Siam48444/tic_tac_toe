ce6281ae6dad1673368f5e1e74d6e9bb2f2e3723
Siam <siam48444@gmail.com>
2025-02-19 16:25:44 +0600

    siam

--- JS/turns.js
+++ JS/turns.js
@@ -6,14 +6,19 @@ export let circle_turn; // Tracks if it's O's turn
 
 
 
-
 // Get the first turn by the user click
 export function get_user_turn() {
     score_button_X.addEventListener("click", choose_X);
     score_button_O.addEventListener("click", choose_O);
 }
-export function choose_X() { circle_turn = false; update_turn_indicator(); }
-export function choose_O() { circle_turn = true; update_turn_indicator(); }
+export function choose_X() { 
+    circle_turn = false; 
+    update_turn_indicator(); 
+}
+export function choose_O() { 
+    circle_turn = true; 
+    update_turn_indicator(); 
+}
 
 
 
