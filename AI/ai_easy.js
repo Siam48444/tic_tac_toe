71953a4778dd3545a663c71fe8bbc056d3ee2286
Siam <siam48444@gmail.com>
2025-02-20 11:57:58 +0600

    siam

--- AI/ai_easy.js
+++ AI/ai_easy.js
@@ -1,9 +1,14 @@
+import { CIRCLE_CLASS, CROSS_CLASS } from "../JS/rules.js";
+
+
 export function get_ai_move(cells) {
-    // Check for the marked cells
+    // Get all available (empty) cells
     const available_cells = [...cells].filter(cell => {
-        return !cell.classList.contains("cross") && !cell.classList.contains("circle");
+        return ( !cell.classList.contains(CROSS_CLASS) && !cell.classList.contains(CIRCLE_CLASS) );
     });
 
-    // Check if there is no cell left
+    // No move if the board is full
     if (available_cells.length === 0) return null;
+
+    console.log(Math.random());
 }
