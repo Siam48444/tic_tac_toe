127c18edd773a05cf471665ca287a3dcdd190dae
Siam <siam48444@gmail.com>
2025-02-20 17:48:05 +0600

    ai

--- AI/ai_easy.js
+++ AI/ai_easy.js
@@ -1,5 +1,7 @@
 import { CIRCLE_CLASS, CROSS_CLASS } from "../JS/rules.js";
-import { circle_turn } from "../JS/turns.js";
+import { circle_turn, place_the_mark, swap_turn, update_turn_indicator } from "../JS/turns.js";
+import { is_draw, is_winner } from "../JS/rules.js";
+import { end_the_game } from "../index.js";
 
 
 export function get_ai_move(cells) {
@@ -15,4 +17,32 @@ export function get_ai_move(cells) {
     const random_index = Math.floor(Math.random() * available_cells.length);
     
     return available_cells[random_index];
+}
+
+
+
+export function place_ai_move(ai_enabled, cells) {
+    // If AI is enabled and it's now AI's turn, make AI move
+    if (ai_enabled && circle_turn) {
+        setTimeout(() => {
+            const ai_cell = get_ai_move(cells);
+
+            if (ai_cell) {
+                place_the_mark(ai_cell, CIRCLE_CLASS);
+
+                if (is_winner(cells, CIRCLE_CLASS)) {
+                    end_the_game(true, is_winner(cells, CIRCLE_CLASS));
+                    return null;
+                }
+                else if (is_draw(cells)) {
+                    end_the_game(false);
+                    return null;
+                }
+                else {
+                    swap_turn();
+                    update_turn_indicator();
+                }
+            }
+        }, 500);
+    }
 }
