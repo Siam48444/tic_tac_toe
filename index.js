cc5fe082ea4c1c675a439eb2aa4a90fd67d891da
Siam <siam48444@gmail.com>
2025-02-21 20:34:31 +0600

    ai

--- index.js
+++ index.js
@@ -22,8 +22,24 @@ let easy_ai_enabled = false;
 mode_selection.addEventListener("change", (e) => {
     let mode = e.target.value;
 
+    if (mode === "easy") {
+        easy_ai_enabled = true;
+        reset_scores();
+        reset_turn();
+        start_the_game();
+    }
+    else if (mode === "two players") {
+        easy_ai_enabled = false;
+        reset_scores();
+        reset_turn();
+        start_the_game();
+    }
+    
+
     // Usual settings for ai mode
     if (mode !== "two players") {
+        disable_turn_selection();
+        
         // Start the next round
         winning_message.addEventListener("click", () => { 
             reset_turn();
@@ -38,21 +54,6 @@ mode_selection.addEventListener("change", (e) => {
             start_the_game();
             disable_turn_selection();
         }); 
-    }
-    
-
-    if (mode === "easy") {
-        easy_ai_enabled = true;
-        reset_scores();
-        reset_turn();
-        start_the_game();
-        disable_turn_selection();
-    }
-    else if (mode === "two players") {
-        easy_ai_enabled = false;
-        reset_scores();
-        reset_turn();
-        start_the_game();
     }
 });
 
