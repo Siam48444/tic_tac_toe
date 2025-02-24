95338001d1bc92605ed6c361cb65832578531914
Siam <siam48444@gmail.com>
2025-02-24 11:59:44 +0600

    ai

--- JS/theme.js
+++ JS/theme.js
@@ -2,7 +2,7 @@ const THEMES = [
     "default_light", "default_dark", "dollar", "serika_dark", "moonlight", 
     "retro", "carbon", "neon_ember", "cafe", "red", "darling", "discord",
     "honey", "leather", "lime", "mexican", "miami", "miami_nights", "modern_ink",
-    "menthol", "metaverse", "olive",
+    "menthol", "metaverse", "olive", "peach",
 ]
 
 const theme_button = document.querySelector(".theme_button");
