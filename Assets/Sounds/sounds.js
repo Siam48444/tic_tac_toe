092bb23864c2ee8dc199254cd96414b3e6d733a5
Siam <siam48444@gmail.com>
2025-02-25 13:06:06 +0600

    ai

--- Assets/Sounds/sounds.js
+++ Assets/Sounds/sounds.js
@@ -6,15 +6,30 @@ export const draw_sound = new Audio("./Assets/Sounds/draw_sound.mp3");
 
 
 
-// Let the user choose to mute or unmute
-export let is_muted = false;
+export let is_muted = false; // Keeps track if the sound is muted or not
 
+
+
+// Check for the sound mode when the page loads
+const sound_muted = localStorage.getItem("sound_muted");
+window.addEventListener("load", () => {
+    if (sound_muted) {
+        is_muted = true;
+        document.getElementById("sound_button").classList.add("muted");
+    }
+});
+
+
+
+// Let the user choose to mute or unmute
 export function set_sound_mode() {
     const sound_button = document.getElementById("sound_button");
 
     sound_button.addEventListener("click", () => {
         is_muted = !is_muted; // Mute or unmute the sound
         sound_button.classList.toggle("muted"); // Show the sound state graphically
+
+        save_sound_mode(); 
     });
 }
 
@@ -24,4 +39,11 @@ export function set_sound_mode() {
 export function play_sound(sound, is_muted = false) {
     if (is_muted) return;
     else sound.play();
+}
+
+
+
+// Save the sound mode in the local storage
+function save_sound_mode() {
+    localStorage.setItem("sound_muted", is_muted);
 }
