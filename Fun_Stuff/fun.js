6e1e677a0f2f63f248572a2e23e46d3ef2e0431e
Siam <siam48444@gmail.com>
2025-02-26 12:49:40 +0600

    ai

--- Fun_Stuff/fun.js
+++ Fun_Stuff/fun.js
@@ -9,6 +9,12 @@ if (theme_class) {
 // Fun button
 const fun_button = document.getElementById("fun_button");
 fun_button.addEventListener("mouseenter", () => {
-    const max_X = window.innerWidth;
-    console.log(max_X)
+    const max_X = window.innerWidth - fun_button.clientWidth;
+    const max_Y = window.innerHeight - fun_button.clientHeight;
+
+    const random_X = Math.random() * max_X;
+    const random_Y = Math.random() * max_Y;
+
+    fun_button.style.left = `${random_X}px`;
+    fun_button.style.top = `${random_Y}px`;
 });
