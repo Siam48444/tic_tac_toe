76c03e7625397a9b85e3f03d8d4e78dcda7627b7
Siam <siam48444@gmail.com>
2025-02-26 12:46:37 +0600

    ai

--- Fun_Stuff/fun.js
+++ Fun_Stuff/fun.js
@@ -3,3 +3,12 @@ const theme_class = localStorage.getItem("theme_class");
 if (theme_class) {
     document.body.classList.add(theme_class);
 }
+
+
+
+// Fun button
+const fun_button = document.getElementById("fun_button");
+fun_button.addEventListener("mouseenter", () => {
+    const max_X = window.innerWidth;
+    console.log(max_X)
+});
