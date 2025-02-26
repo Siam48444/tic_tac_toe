50b0a7044cff78a68cea332568ba2d285292f996
Siam <siam48444@gmail.com>
2025-02-26 13:20:54 +0600

    ai

--- Fun_Stuff/fun.js
+++ Fun_Stuff/fun.js
@@ -13,16 +13,16 @@ const fun_button = document.getElementById("fun_button");
 const padding = 30; // Space between the button and the edges
 
 // Make the button tricky to click
-// fun_button.addEventListener("mouseenter", () => {
-//     const max_X = main.clientWidth - fun_button.clientWidth - padding;
-//     const max_Y = main.clientHeight - fun_button.clientHeight - padding;
+fun_button.addEventListener("mouseenter", () => {
+    const max_X = main.clientWidth - fun_button.clientWidth - padding;
+    const max_Y = main.clientHeight - fun_button.clientHeight - padding;
 
-//     const random_X = padding + Math.random() * max_X;
-//     const random_Y = padding + Math.random() * max_Y;
+    const random_X = padding + Math.random() * max_X;
+    const random_Y = padding + Math.random() * max_Y;
 
-//     fun_button.style.left = `${random_X}px`;
-//     fun_button.style.top = `${random_Y}px`;
-// });
+    fun_button.style.left = `${random_X}px`;
+    fun_button.style.top = `${random_Y}px`;
+});
 
 // Interactive button clicks
 fun_button.addEventListener("click", () => {
